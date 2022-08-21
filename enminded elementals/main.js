e1af00d9a7b6fde45f46ea73d14d747a56b12b94
poofsjawn <poofsjawn@gmail.com>
2022-08-21 03:02:46 -0400

    test upload blender file

--- enminded elementals/main.js
+++ enminded elementals/main.js
@@ -3,26 +3,40 @@ import './style.css'
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+const loader = new GLTFLoader();
+
+
 
 const scene = new THREE.Scene();
-
+scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGL1Renderer({
-    canvas:document.querySelector('#bg'),
-})
+const renderer = new THREE.WebGL1Renderer();
+document.body.appendChild( renderer.domElement );
 
-renderer.setPixelRatio( window.devicePixelRatio );
+
 renderer.setSize( window.innerWidth, window.innerHeight );
 
-camera.position.setZ(30);
+camera.position.setZ(5);
 
 renderer.render( scene, camera);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1);
+loader.load( 'jar2.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
+const geometry =  new THREE.CylinderGeometry( 5, 5, 10, 36 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cylinder = new THREE.Mesh( geometry, material );
+scene.add( cylinder );
 const controls = new OrbitControls( camera, renderer.domElement );
 
 camera.position.z = 5;
@@ -32,9 +46,9 @@ controls.update();
 //loops like p5
 function animate() {
 	requestAnimationFrame( animate );
-   //cube.rotation.x += 0.01;
+  //cube.rotation.x += 0.01;
   //cube.rotation.y += 0.01;
-    controls.update();
+  controls.update();
 
 	renderer.render( scene, camera );
 }
